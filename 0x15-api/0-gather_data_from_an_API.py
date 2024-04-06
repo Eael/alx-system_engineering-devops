bb4305ab620c0c21f5fbdc318cfc7c97b93133e2
Sandbox 1 <1kwameearl@gmail.com>
2024-04-06 13:25:01 -0700

    Revert "0-gather_data_from_an_API.py"
    This reverts commit 749cefdfde9e74c6aa9f4b5998bba9ba13b838a7.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,21 @@
 import requests
 import sys
 
-if __name__ = "__main__":
+
+def to_do(id):
     """Script that displays employee TODO list
         Parameters:
         id: an integer representing employee id
     """
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(id)).json()
+    todos = requests.get(url + "todos", params={"userId": id}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
+
+
+if __name__ == '__main__':
+    to_do(sys.argv[1])
