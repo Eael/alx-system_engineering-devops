824eadc6db284412cfd7a46bac75292c95439637
Sandbox 1 <1kwameearl@gmail.com>
2024-04-06 13:29:18 -0700

    new 2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,16 @@ import requests
 import sys
 
 
-def to_do(id):
+if __name__ == '__main__':
     """Script that displays employee TODO list
         Parameters:
         id: an integer representing employee id
     """
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(id)).json()
-    todos = requests.get(url + "todos", params={"userId": id}).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
-
-if __name__ == '__main__':
-    to_do(sys.argv[1])
