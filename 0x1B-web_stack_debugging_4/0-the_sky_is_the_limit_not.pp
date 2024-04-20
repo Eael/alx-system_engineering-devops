4078244555b60c0f18238ac259659ef81918a5f0
PuppetBox <1kwameearl@gmail.com>
2024-04-20 14:57:35 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix nginx to accept and serve more requests
 
 exec {'modify max open files limit setting':
-  command => 'sudo sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-  provider    => 'shell',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  provider => 'shell',
 }
